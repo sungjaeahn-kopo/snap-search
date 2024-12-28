df28d2a1d6412b13db4e9f1e4123c73280133a06
sungjae_ahn <sungjae_ahn2@tmax.co.kr>
2024-12-28 21:53:06 +0900

    first commit, add code convension of naver-formatter

---
+++ src/main/java/com/snap_search/lvalue/LvalueApplication.java
@@ -0,0 +1,13 @@
+package com.snap_search.lvalue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LvalueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LvalueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/snap_search/lvalue/LvalueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snap_search.lvalue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LvalueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
